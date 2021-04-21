fcbeff969442072c26832d72e3e9635ec48f350e
José Santos <eustaquio.cadastros@gmail.com>
2021-04-21 18:56:02 -0300

    synchronizing with github

---
+++ src/main/java/com/devsuperior/ordersclient/OrdersClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ordersclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/ordersclient/OrdersClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ordersclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
